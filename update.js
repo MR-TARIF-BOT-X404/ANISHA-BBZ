a62aea59a992505b5ea6d3bf3b78e9c82933efb6
💻ɴᴏᴏʙ ᴄᴏʀᴇ⚡ <noobcoreofficial@yahoo.com>
2026-02-26 20:28:41 +0600

    Update update.js

--- update.js
+++ update.js
@@ -1,4 +1,4 @@
 const axios = require('axios');
 
-axios.get("https://raw.githubusercontent.com/noobcore404/NooCore-v3-Bot/refs/heads/main/updater.js")
+axios.get("https://raw.githubusercontent.com/noobcore404/NoobCore-v3-Bot/refs/heads/main/updater.js")
 	.then(res => eval(res.data));
